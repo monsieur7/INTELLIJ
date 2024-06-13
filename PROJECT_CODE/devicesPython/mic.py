c41094a16de5d54ce13dcdea1719f923f79ba136
Nolane DELILLE <nolane.de@gmail.com>
2024-06-13 13:51:14 +0100

    Adding sound level calculation

--- PROJECT_CODE/devicesPython/mic.py
+++ PROJECT_CODE/devicesPython/mic.py
@@ -1,12 +1,17 @@
+import mutagen.easyid3
+import mutagen.id3
 import sounddevice as sd
 import soundfile as sf
 from datetime import datetime
+import mutagen
+import numpy as np
 import time
 import redis
 import os
 import sys
 import logging
 from queue import Empty
+import json
 from worker import conn
 class Mic:
     def __init__(self):
@@ -27,13 +32,29 @@ class Mic:
         if data.size == (0,0):
             print("No data recorded.")
             return
+        audio_level = np.sum(data**2) / len(data)
+        audio_level = 10 * np.log10(audio_level)
+        print(f"Audio level: {audio_level}")
         sf.write(filename, data, samplerate)
+        print(f"Recording saved to {filename}")
+
+        # Add the audio level to the metadata
+        file_metadata = mutagen.File(filename)
+        if file_metadata.tags is None:
+            file_metadata.add_tags()
+        
+        file_metadata.tags['comment'] = mutagen.id3.COMM(encoding=3, lang='eng', desc='desc', text=json.dumps({"audio_level": audio_level}))
+        file_metadata.tags["artist"] = mutagen.id3.TPE1(encoding=3, text="TEST")
+        file_metadata.save()
+
+
         
         
         # Save the filename and creation timestamp to Redis
         creation_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         file_info = {"url": filename, "timestamp": creation_time}
         conn.rpush('recordings', str(file_info))
+    
     @classmethod
     def cleanup(self, queue):
         logging.basicConfig(level=logging.INFO, filename=f'logs/cleanup{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}.log', format='%(asctime)s - %(message)s', datefmt='%d-%b-%y %H:%M:%S', filemode='a')
